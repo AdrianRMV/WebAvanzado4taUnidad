3ceed298f7b1f2df6e0269f5d84dce9860955c3c
Adrian <adrianthelawx@gmail.com>
2022-09-22 18:51:09 -0600

    Color correjido xd

--- products/index.php
+++ products/index.php
@@ -116,7 +116,7 @@
                     <div class="dropdown pb-4">
                         <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                             <img src="https://github.com/AdrianRMV.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
-                            <span class="d-none d-sm-inline mx-1">Adrian</span>
+                            <span class="d-none d-sm-inline mx-1" style="color: #000 !important">Adrian</span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
                             <li><a class="dropdown-item" href="#">New project...</a></li>
